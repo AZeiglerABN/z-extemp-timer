af3e8c74b68ccd5f1ea7326827106b96159f40bd
AZeiglerABN <andyzeig@gmail.com>
2026-03-05 17:55:33 -0600

    Update cache name to 'z-extemp-timer-v2'

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'z-extemp-timer-v1';
+const CACHE_NAME = 'z-extemp-timer-v2';
 const ASSETS = ['./', './index.html', './logo.png', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
