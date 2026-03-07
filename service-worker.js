9857a17beb9c38beaf5ffd07818f3af72708b7db
AZeiglerABN <andyzeig@gmail.com>
2026-03-07 11:32:07 -0600

    Add Debate Logo Black.png to service worker assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'z-extemp-timer-v3';
-const ASSETS = ['./', './index.html', './logo.png', './manifest.webmanifest'];
+const ASSETS = ['./', './index.html', './logo.png', './Debate Logo Black.png', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
